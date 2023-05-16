ffedd2a5b35fa43e45c32b7462dc11c89402c2da
marcelinho0938 <marcelinho.sp11@gmail.com>
2023-05-16 11:25:14 -0300

    matriz arrumada

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-(function() {
-
-    var tabela = document.querySelector('[data-js="tbody"]')
+var tabela = document.querySelector('[data-js="tbody"]')
+let main = document.querySelector('#exibir')
 
     function selectID(linha, coluna){
         var tr = tabela.children[linha]
@@ -8,14 +7,8 @@
         td.classList.add("color")
     }
 
-    var lines = []
-
-    letters.map(function(item, index){
-        lines[index] = document.querySelector('[data-js="linha' + index + '"]')
-    })
-
-
-    var p = [
+   
+    var jogo = [
         ['a', 'j', 'f', 'k', 'l', 'm'],
         ['b', 'c', 'i', 'h', 'd', 'z'],
         ['j', 'b', 'l', 't', 'c', 'w'],
@@ -23,7 +16,25 @@
         ['u', 'n', 'o', 'v', 'x', 'r'],
     ]
 
-})
+    var lines = []
+
+    
+    const retornoMap =  jogo.map((nomeAtual) => {
+        console.log(nomeAtual);
+    });
+
+    jogo.forEach((item) => {
+        jogo.forEach(function(){
+            lines.insertAdjacentHTML('beforeend', '<td>' + item + '</td>')
+        })
+    })
+
+main.innerHTML = jogo
+
+
+    
+
+
 
 
 
